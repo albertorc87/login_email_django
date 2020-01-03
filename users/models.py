81165ec8a1b65d027386154bea17db1cb17b94a5
Alberto <alberto.r.caballero.87@gmail.com>
2020-01-03 17:51:34 +0100

    first commit

---
+++ users/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+
+    email = models.EmailField('email address', unique=True)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
